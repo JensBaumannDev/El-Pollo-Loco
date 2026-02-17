634fff7ce8c3ab27d41800255b2c4a8d9eaa222c
JensBaumannDev <baumannjens@outlook.de>
2026-02-18 00:15:48 +0100

    added bew movable-object.class.js - create new MovableObject

--- js/game.js
+++ js/game.js
@@ -1,11 +1,12 @@
 let canvas;
 let ctx;
-let charakter = new Image();
+let charakter = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    
-    charakter.src = '../img/2_character_pepe/2_walk/W-21.png';
-    ctx.drawImage(charakter, 20, 20, 50, 150);
+
+console.log('My Charakter is', charakter);
+
+
 }

3eb528c722ee005c5fe745df7ccbcb0b6a484633
JensBaumannDev <baumannjens@outlook.de>
2026-02-20 03:43:25 +0100

    change height and width of chicken-enemies to 90

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject {
-    y = 360;
-    height = 60;
-    width = 60;
+    y = 330;
+    height = 90;
+    width = 90;
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',

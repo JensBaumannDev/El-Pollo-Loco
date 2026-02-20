8db709b20868f77e471de4cb8ac542e144dca1bb
JensBaumannDev <baumannjens@outlook.de>
2026-02-20 01:42:28 +0100

    added mirror animation if pepe walks to the left

--- models/world.class.js
+++ models/world.class.js
@@ -26,7 +26,7 @@ class World {
         this.setWorld();
     }
 
-    setWorld(){
+    setWorld() {
         this.character.world = this;
     }
 
@@ -52,7 +52,18 @@ class World {
     }
 
     addToMap(mo) {
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-    }
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1)
+            mo.x = mo.x * -1;
+        }
 
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
+    }
 }

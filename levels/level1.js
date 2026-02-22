2137696aa10f2e60172542c6298b148461330f13
JensBaumannDev <baumannjens@outlook.de>
2026-02-22 01:05:52 +0100

    change enemies to 3 chicken

--- levels/level1.js
+++ levels/level1.js
@@ -2,10 +2,7 @@ const level1 = new Level(
     [
         new Chicken(),
         new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Endboss()
+        new Chicken()
     ],
     [
         new Cloud()
